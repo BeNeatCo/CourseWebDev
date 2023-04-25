440b97c46a49fc96707e46494a1adba2736e0562
jhavb@rknec.edu <vineetjha372@gmail.com>
2023-04-25 23:13:50 +0530

    Edit1

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-function toggleHikingList() {
-    var hikingList = document.querySelector("#hiking-list");
-    if (hikingList.style.display === "none") {
-      hikingList.style.display = "block";
-    } else {
-      hikingList.style.display = "none";
-    }
-  }
-  
+// Smooth scrolling
+$(document).ready(function(){
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+});
