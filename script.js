cf0cfa84ea8efd963efc20f6cc63cbd5dfc2ffac
jhavb@rknec.edu <vineetjha372@gmail.com>
2023-04-25 22:45:50 +0530

    New Repo

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleHikingList() {
+    var hikingList = document.querySelector("#hiking-list");
+    if (hikingList.style.display === "none") {
+      hikingList.style.display = "block";
+    } else {
+      hikingList.style.display = "none";
+    }
+  }
+  
